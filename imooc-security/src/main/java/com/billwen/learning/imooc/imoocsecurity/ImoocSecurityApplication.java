a89225a701fe7fc76279f203a11e2b9a55a2efca
Gang Wen <gloriatanjm@hotmail.com>
2022-05-21 09:38:31 +0800

    Generated skeleton of the project

---
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/ImoocSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.billwen.learning.imooc.imoocsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocSecurityApplication.class, args);
+	}
+
+}
---
+++ imooc-security/src/test/java/com/billwen/learning/imooc/imoocsecurity/ImoocSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billwen.learning.imooc.imoocsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
