9090bcd00508c081ddcaa4e9a43e4fc3ff079c04
Gang Wen <gloriatanjm@hotmail.com>
2022-05-21 14:06:02 +0800

    2-1 Done
    Completed 2-1

---
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.billwen.learning.imooc.imoocsecurity.config;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/api/*")
+                .authenticated();
+
+        http.formLogin(Customizer.withDefaults());
+
+    }
+}
