4a6552ace715b03494fa8c6429f66f9624a5d1ba
Gang Wen <gloriatanjm@hotmail.com>
2022-05-22 13:12:44 +0800

    2-6 定制登录页面

--- imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/config/SecurityConfig.java
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.billwen.learning.imooc.imoocsecurity.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,10 +13,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/api/*")
+                .mvcMatchers("/api/**")
                 .authenticated();
 
-        http.formLogin(Customizer.withDefaults());
+
+        http.formLogin()
+            .loginPage("/login")
+            .permitAll();
 
         http.httpBasic(Customizer.withDefaults());
 
@@ -26,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().mvcMatchers("/public/**");
+        web.ignoring().mvcMatchers("/public/**")
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
