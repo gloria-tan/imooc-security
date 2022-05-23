c3fe41d6681f9718946231aa368d72caa4a80b94
Gang Wen <gloriatanjm@hotmail.com>
2022-05-23 18:19:57 +0800

    Squashed commit of the following:
    commit 1309b542d0b7a3c55401929a4aa24b79a41b1a7e
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Mon May 23 14:00:10 2022 +0800
    
        2-9 自定义Filter
    
    commit 55255d4ffbc2288837cb9dc8e21f17a63627a7a9
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Mon May 23 11:02:18 2022 +0800
    
        2-8 登录成功和失败的处理
    
    commit 37f27612776c19eea99d74b9c7f35f20d310ce65
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sun May 22 15:56:36 2022 +0800
    
        2-7 csrf, logout 和remember-me的设置
    
    commit 4a6552ace715b03494fa8c6429f66f9624a5d1ba
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sun May 22 13:12:44 2022 +0800
    
        2-6 定制登录页面
    
    commit 847805b396d33eb91aa1e20e073a084178999dc8
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sun May 22 09:48:59 2022 +0800
    
        2-5 Done
    
    commit d2a40173cc51549814e5991d73a9735b12402490
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sun May 22 09:11:17 2022 +0800
    
        2-5 Done
    
    commit fd1a3a25809f7d0adc00fc00dd1d6cd34635ffea
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sun May 22 08:52:54 2022 +0800
    
        2-3 Done
    
    commit b4c51aa34eb26e584f907249ef88ef595be5c7d1
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sat May 21 17:13:13 2022 +0800
    
        2-2 Done
    
    commit 9090bcd00508c081ddcaa4e9a43e4fc3ff079c04
    Author: Gang Wen <gloriatanjm@hotmail.com>
    Date:   Sat May 21 14:06:02 2022 +0800
    
        2-1 Done
    
        Completed 2-1

---
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.billwen.learning.imooc.imoocsecurity.config;
+
+import com.billwen.learning.imooc.imoocsecurity.security.filter.RestAuthenticationFilter;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/authorize/login").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .mvcMatchers("/api/**").hasRole("USER")
+                .anyRequest().authenticated();
+
+
+        http.formLogin()
+                .successHandler(new RestAuthenticationSuccessHandler())
+                .failureHandler(new RestAuthenticationFailureHandler())
+                .loginPage("/login")
+                .permitAll();
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf()
+                .csrfTokenRepository(new CookieCsrfTokenRepository())
+                .ignoringAntMatchers("/api/**", "/authorize/**");
+
+        http.rememberMe()
+            .tokenValiditySeconds(30 * 24 * 3600)
+            .rememberMeCookieDomain("somethingToRemember");
+
+        http.logout()
+                .logoutSuccessHandler(new RestLogoutSuccessHandler())
+                .invalidateHttpSession(true)
+                .deleteCookies();
+
+        http.addFilterBefore(restAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/public/**", "/error")
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("12345678"))
+                .authorities("ROLE_USER", "ROLE_ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private RestAuthenticationFilter restAuthenticationFilter() throws Exception {
+        RestAuthenticationFilter filter = new RestAuthenticationFilter();
+        filter.setAuthenticationSuccessHandler(new RestAuthenticationSuccessHandler());
+        filter.setAuthenticationFailureHandler(new RestAuthenticationFailureHandler());
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setFilterProcessesUrl("/authorize/login");
+
+        return filter;
+    }
+}
---
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.billwen.learning.imooc.imoocsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/")
+                .resourceChain(false);
+
+        registry.setOrder(1);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login")
+                .setViewName("loginPage");
+
+        registry.addViewController("/").setViewName("index");
+
+        registry.setOrder(1);
+    }
+}
