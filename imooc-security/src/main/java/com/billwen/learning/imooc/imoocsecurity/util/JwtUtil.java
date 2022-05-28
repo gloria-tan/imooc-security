5d23e0aafdbcab988df2e555afdbf88733e1995b
Gang Wen <gloriatanjm@hotmail.com>
2022-05-28 11:39:33 +0800

    5-3 JWT的概念和创建以及解析

---
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/util/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.billwen.learning.imooc.imoocsecurity.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtil {
+    public static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public String createJwtToken(UserDetails userDetails) {
+        var now = System.currentTimeMillis();
+
+        return Jwts.builder()
+                .setId("mooc")
+                .claim("authorities", userDetails.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.toList()))
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + 60_000))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+}
---
+++ imooc-security/src/test/java/com/billwen/learning/imooc/imoocsecurity/util/JwtUtilTest.java
@@ -0,0 +1,41 @@
+package com.billwen.learning.imooc.imoocsecurity.util;
+
+import com.billwen.learning.imooc.imoocsecurity.domain.Role;
+import com.billwen.learning.imooc.imoocsecurity.domain.User;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+public class JwtUtilTest {
+
+    JwtUtil jwtUtil;
+
+    @BeforeEach
+    public void setup() {
+        this.jwtUtil = new JwtUtil();
+    }
+
+    @Test
+    public void givenUserDetails_thenCreateTokenSuccess() {
+        String username = "user";
+        var authorities = Set.of(Role.builder().authority("ROLE_USER").build(),
+                Role.builder().authority("ROLE_ADMIN").build());
+
+        var user = User.builder().username(username).authorities(authorities).build();
+
+        var token = jwtUtil.createJwtToken(user);
+
+        var parsedClaims = Jwts.parserBuilder().setSigningKey(jwtUtil.key).build().parseClaimsJws(token).getBody();
+
+        assertEquals(username, parsedClaims.getSubject(), "解析后 Subject 应是用户名");
+    }
+}
