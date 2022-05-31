004e33a28eeefca218025bd25e22b73af7b5f3f8
Gang Wen <gloriatanjm@hotmail.com>
2022-05-31 15:10:45 +0800

    6-1 多因子认证和TOTP

---
+++ imooc-security/src/main/java/com/billwen/learning/imooc/imoocsecurity/util/TotpUtil.java
@@ -0,0 +1,92 @@
+package com.billwen.learning.imooc.imoocsecurity.util;
+
+import com.eatthepath.otp.TimeBasedOneTimePasswordGenerator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TotpUtil {
+
+    public static final long TIME_STEP = 60 * 5L;
+
+    private static final int PASSWORD_LENGTH = 6;
+
+    private KeyGenerator keyGenerator;
+
+    private TimeBasedOneTimePasswordGenerator totp;
+
+    {
+        try {
+            this.totp = new TimeBasedOneTimePasswordGenerator(Duration.ofSeconds(TIME_STEP), PASSWORD_LENGTH);
+            this.keyGenerator = KeyGenerator.getInstance(totp.getAlgorithm());
+            // SHA-1 and SHA-256 需要64字节(512 位)的key
+            // SHA 512 需要128字节（1024 位）的key
+            this.keyGenerator.init(512);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("没有找到算法 {}", e.getLocalizedMessage());
+
+        }
+    }
+
+    /**
+     * t
+     * @param key
+     * @param time
+     * @return
+     * @throws InvalidKeyException
+     */
+    public String createTotp(Key key, Instant time) throws InvalidKeyException {
+        int password = totp.generateOneTimePassword(key, time);
+        String format = "%0" + PASSWORD_LENGTH + "d";
+        return String.format(format, password);
+    }
+
+    /**
+     * 验证 TOTP key
+     * @param key
+     * @param code
+     * @return
+     * @throws InvalidKeyException
+     */
+    public boolean verifyTotp(Key key, String code) throws InvalidKeyException {
+        var now = Instant.now();
+        return code.equals(createTotp(key, now));
+    }
+
+    public Key generateKey() {
+        return this.keyGenerator.generateKey();
+    }
+
+    public String encodeKeyToString(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public String encodeKeyToString() {
+        return encodeKeyToString(generateKey());
+    }
+
+    public Key decodeKeyFromString(String stringifiedKey) {
+        return new SecretKeySpec(Base64.getDecoder().decode(stringifiedKey), this.totp.getAlgorithm());
+    }
+
+    public Optional<String> createTotp(String stringifiedKey) {
+        try {
+            return Optional.of(createTotp(decodeKeyFromString(stringifiedKey), Instant.now()));
+        } catch (InvalidKeyException e) {
+            return Optional.empty();
+        }
+    }
+}
---
+++ imooc-security/src/test/java/com/billwen/learning/imooc/imoocsecurity/util/TotpUtilTest.java
@@ -0,0 +1,50 @@
+package com.billwen.learning.imooc.imoocsecurity.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.security.Key;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+public class TotpUtilTest {
+
+    private TotpUtil totpUtil;
+
+    @BeforeEach
+    private void setup() {
+        this.totpUtil = new TotpUtil();
+    }
+
+    @Test
+    public void givenSameKeyAndTotp_whenValidateTwice_thenFail() throws Exception {
+        Instant now = Instant.now();
+        Instant validFuture = now.plusSeconds(TotpUtil.TIME_STEP);
+        Key key = totpUtil.generateKey();
+        String firstTotp = this.totpUtil.createTotp(key, now);
+        Key secondKey = this.totpUtil.generateKey();
+        assertTrue(totpUtil.verifyTotp(key, firstTotp), "第一次验证成功");
+
+        String secondTotp = this.totpUtil.createTotp(key, Instant.now());
+        assertEquals(firstTotp, secondTotp, "时间间隔内生成的两个TOTP是一致的");
+
+        String afterTimeStepTotp = totpUtil.createTotp(key, validFuture);
+        assertNotEquals(firstTotp, afterTimeStepTotp, "过期之后和原来的TOTP比较应该不一致");
+        assertFalse(totpUtil.verifyTotp(secondKey, firstTotp), "使用新的key验证原来的TOTP应该失败");
+    }
+
+    @Test
+    public void givenKey_ThenEncodeAndDecodeSuccess() {
+        Key key = this.totpUtil.generateKey();
+        String stringifiedKey = this.totpUtil.encodeKeyToString(key);
+        Key decodeKey = this.totpUtil.decodeKeyFromString(stringifiedKey);
+        assertEquals(key, decodeKey, "从字符串中获得的 Key 解码后应该和原来的 key 一致。");
+    }
+}
